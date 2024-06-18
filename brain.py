39046f9aeea75d868e72189f4a7aaf913310c923
Shreyas-Alse <77925552+Shreyas-Alse@users.noreply.github.com>
2024-06-18 17:17:29 +0530

    Add files via upload

--- brain.py
+++ brain.py
@@ -11,14 +11,14 @@ def talk(text):
     engine.runAndWait()
 
 
-client = Groq(api_key='YOUR API KEY' )
+client = Groq(api_key='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' )
 
 # Set the system prompt
 system_prompt = {
     "role": "system",
     "content": ""}
 
-file_path = '/path/to/personality.txt'
+file_path = '/home/shreyas/Ultron/personality.txt'
 
 with open(file_path, 'r') as file:
     file_content = file.read()
@@ -50,6 +50,29 @@ def respond(prompt):
 
 
 
+writer_mode = {
+    "role": "system",
+    "content": 'You are a professional writer and write documents on the given topic. No other reponses are to be given. Write elaborately along with links for future further references'}
+
+writer_history= [writer_mode]
+
+    
+def prep_doc(topic):
+
+  # Append the user input to the chat history
+    writer_history.append({"role": "user", "content": topic})
+
+    response = client.chat.completions.create(model="llama3-70b-8192",
+                                            messages=writer_history,
+                                            max_tokens=10000,
+                                            temperature=1.2)
+  # Append the response to the chat history
+    writer_history.append({
+            "role": "Professional Writer",
+            "content": response.choices[0].message.content
+        })
+    doc_content= (response.choices[0].message.content)
+    return doc_content
 
 
 
